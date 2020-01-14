71f3cefab8b1cb251e0f987b0798ad10d37ed242
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2020-01-14 11:46:13 -0500

    Created tmp_render_order_submenu_page.php Moved render order submenu html generating php function to aforementioned file Submenu still working

--- index.php
+++ index.php
@@ -20,8 +20,9 @@ $plugin_path	= dirname( __FILE__ );
 //exit;
 
 
-
+// Bring in code from outside files
 include $plugin_path . '/tmp_list_page.php';
+include $plugin_path . '/tmp_render_order_submenu_page.php';
 
 
 
@@ -71,32 +72,6 @@ add_theme_support( 'post-thumbnails' );
 
 
 
-// This code generates a submenu for the Team Members plugin that will handle profile render order
-function tmp_render_order_submenu_html(){
-	// check user capabilities
-	if (!current_user_can('manage_options')) {
-		return;
- 	}
-	
- 
-?>
-	<div class="wrap">
-		<h1><?= esc_html(get_admin_page_title()); ?></h1>
-		<form action="options.php" method="post">
-		<h1>This is where we echo out profiles by order, let users drag-and-drop rearrange, then save that new order (somehow)</h1>
-<?php
-		// output security fields for the registered setting "wporg_options"
-		//settings_fields('wporg_options');
-		// output setting sections and their fields
-		// (sections are registered for "wporg", each field is registered to a specific section)
-		//do_settings_sections('wporg');
-		// output save settings button
-		//submit_button('Save Settings');
-?>
-		</form>
-	</div>
-<?php
-}
 
 
 function tmp_render_order_submenu()
